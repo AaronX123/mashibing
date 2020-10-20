97545df292abcc76e2f69ed0681efcd90daf98a9
xiaoyouming <739243573@qq.com>
2020-10-20 19:00:28 +0800

    1

---
+++ src/main/java/thread/Synchronized.java
@@ -0,0 +1,88 @@
+package thread;
+
+/**
+ * @author xiaoyouming
+ * @version 1.0
+ * @since 2020-10-20
+ */
+public class Synchronized {
+    // -------------------------------这里演示锁对象---------------------------------//
+    private static final Object LOCK = new Object();
+
+    public void functionA(){
+        synchronized (LOCK){
+            for (int i = 0; i < 100; i++) {
+                System.out.println("a got lock");
+            }
+        }
+    }
+
+    public void functionB(){
+        synchronized (LOCK){
+            for (int i = 0; i < 100; i++) {
+                System.out.println("b got lock");
+            }
+        }
+    }
+
+    // -------------------------------这里演示锁自身-------------------------------------//
+
+    public void functionC(){
+        synchronized (this){
+            for (int i = 0; i < 100; i++) {
+                System.out.println("c got lock");
+            }
+        }
+    }
+
+    public synchronized void functionD(){
+        for (int i = 0; i < 100; i++) {
+            System.out.println("d got lock");
+        }
+    }
+
+
+    // --------------------------------这里演示锁Class对象-------------------------------------//
+
+
+    public static synchronized void functionE(){
+        for (int i = 0; i < 100; i++) {
+            System.out.println("e got lock");
+        }
+    }
+
+    public void functionF(){
+        synchronized (Synchronized.class){
+            for (int i = 0; i < 100; i++) {
+                System.out.println("f got lock");
+            }
+        }
+    }
+
+
+
+    // --------------------------------------------------------------------------------------//
+
+
+    public static void main(String[] args) {
+        Synchronized s = new Synchronized();
+        Thread a = new Thread(s::functionA);
+
+        Thread b = new Thread(s::functionB);
+
+        Thread c = new Thread(s::functionC);
+
+        Thread d = new Thread(s::functionD);
+
+        Thread e = new Thread(Synchronized::functionE);
+
+        Thread f = new Thread(s::functionF);
+
+        //a.start();
+        //b.start();
+        //c.start();
+        //d.start();
+        e.start();
+        f.start();
+    }
+}
---
+++ src/main/java/thread/Volatile.java
@@ -0,0 +1,75 @@
+package thread;
+
+/**
+ * @author xiaoyouming
+ * @version 1.0
+ * @since 2020-10-20
+ */
+public class Volatile {
+    private static volatile int a = 0;
+
+    public void funcA(){
+        for (int i = 0; i < 10000; i++) {
+            a++;
+        }
+    }
+
+    public void funcB(){
+        for (int i = 0; i < 10000; i++) {
+            a++;
+        }
+    }
+
+
+    public static void funcC(){
+        for (int i = 0; i < 100000; i++) {
+            a++;
+        }
+    }
+
+    public static void funcD(){
+        for (int i = 0; i < 100000; i++) {
+            a++;
+        }
+    }
+
+//    public static void main(String[] args) throws InterruptedException {
+////        Volatile v = new Volatile();
+////        Thread ta = new Thread(v::funcA);
+////        Thread tb = new Thread(v::funcB);
+////        ta.start();
+////        tb.start();
+////        ta.join();
+////        tb.join();
+////        System.out.println(a);
+//
+//        new Thread(Volatile::funcC).start();
+//        new Thread(Volatile::funcD).start();
+//
+//        Thread.sleep(2000);
+//        System.out.println(a);
+//    }
+
+
+    // ---------------------------------------------------------------------//
+
+
+    private static volatile boolean flag = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            System.out.println("oooo");
+            while (flag){
+            }
+            System.out.println("oooo");
+        });
+
+        thread.start();
+
+        Thread.sleep(500);
+        System.out.println("flag set to false");
+        flag = false;
+
+    }
+
+}
