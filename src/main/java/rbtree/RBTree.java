d0e7f7b1c50dd34ea535a3ca81f96122be91cc75
xiaoyouming <739243573@qq.com>
2020-11-06 17:46:39 +0800

    rbt

---
+++ src/main/java/rbtree/RBTree.java
@@ -0,0 +1,174 @@
+package rbtree;
+
+import java.util.Objects;
+
+/**
+ * @author xiaoyouming
+ * @version 1.0
+ * @since 2020-11-06
+ */
+public class RBTree<T> {
+    private class TreeNode<T> {
+        T val;
+        boolean red;
+        TreeNode<T> left;
+        TreeNode<T> right;
+        TreeNode<T> parent;
+
+        TreeNode(T val, TreeNode<T> parent) {
+            this.val = val;
+            this.red = true;
+            this.left = null;
+            this.right = null;
+            this.parent = parent;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            TreeNode<?> treeNode = (TreeNode<?>) o;
+            return Objects.equals(val, treeNode.val);
+        }
+
+        @Override
+        public int hashCode() {
+            return val.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", red=" + red +
+                    '}';
+        }
+    }
+
+    private TreeNode<T> root;
+    private int size;
+
+    public RBTree() {}
+
+    public void insert(T val) {
+        TreeNode<T> x;
+        if (root == null) {
+            root = x = new TreeNode<>(val,null);
+        } else {
+            TreeNode<T> insertNode = find(val,root);
+            x = insert0(insertNode,val);
+        }
+        reBalanceAfterInsert(x);
+        size++;
+    }
+
+    private TreeNode<T> find(T target, TreeNode<T> cur) {
+        if (cur == null) {
+            return null;
+        }
+        int ch = cur.hashCode();
+        int th = target.hashCode();
+        TreeNode<T> next;
+        if (ch <= th) {
+            return (next = find(target,cur.right)) == null ? cur : next;
+        } else {
+            return (next = find(target,cur.left)) == null ? cur : next;
+        }
+    }
+
+    private TreeNode<T> insert0(TreeNode<T> insertPoint, T val) {
+        TreeNode<T> n = new TreeNode<>(val,insertPoint);
+        if (insertPoint.hashCode() <= n.hashCode()) {
+            insertPoint.right = n;
+        } else {
+            insertPoint.left = n;
+        }
+        return n;
+    }
+
+    private void reBalanceAfterInsert(TreeNode<T> x) {
+        if (root == x) {
+            x.red = false;
+            return;
+        }
+        TreeNode<T> xp = x.parent;
+        TreeNode<T> xpp = xp.parent;
+        // 如果父节点是黑色或者祖父节点不存在直接结束调整
+        if (!xp.red || xpp == null) {
+            return;
+        }
+        TreeNode<T> uncle = xpp.left == xp ? xpp.right : xpp.left;
+        if (inline(x,xp,xpp)) {
+            // 如果叔叔节点存在且为红色，则xp和uncle变黑xpp变红，同时考虑对更大一层的影响
+            if (uncle != null && uncle.red) {
+                xp.red = false;
+                uncle.red = false;
+                xpp.red = true;
+                reBalanceAfterInsert(xpp);
+                return;
+            }
+            handleInline(x,xp,xpp);
+            return;
+        }
+        if (typeZ(x,xp,xpp)) {
+            if (xpp.right == xp) {
+                xpp.right = x;
+                x.right = xp;
+            } else {
+                xpp.left = x;
+                x.left = xp;
+            }
+            x.parent = xpp;
+            xp.parent = x;
+            handleInline(xp,x,xpp);
+        }
+    }
+
+    private boolean inline(TreeNode<T> x, TreeNode<T> xp, TreeNode<T> xpp) {
+        return (xpp.left == xp && xp.left == x) || (xpp.right == xp && xp.right == x);
+    }
+
+    private boolean typeZ(TreeNode<T> x, TreeNode<T> xp, TreeNode<T> xpp) {
+        return (xpp.left == xp && xp.right == x) || (xpp.right == xp && xp.left == x);
+    }
+
+    private void handleInline(TreeNode<T> x, TreeNode<T> xp, TreeNode<T> xpp) {
+        if (xp.right == x) {
+            if (xpp.parent != null) {
+                xpp.parent.right = xp;
+                xp.parent = xpp.parent;
+            }
+            xpp.parent = xp;
+            if (xpp == root) {
+                root = xp;
+                root.parent = null;
+            }
+            xpp.right = xp.left;
+            if (xp.left != null) {
+                xp.left.parent = xpp;
+            }
+            xp.left = xpp;
+        } else {
+            if (xpp.parent != null) {
+                xpp.parent.left = xp;
+                xp.parent = xpp.parent;
+            }
+            xpp.parent = xp;
+            if (xpp == root) {
+                root = xp;
+                root.parent = null;
+            }
+            xpp.left = xp.right;
+            if (xp.right != null) {
+                xp.right.parent = xpp;
+            }
+            xp.right = xpp;
+        }
+        xp.red = false;
+        xpp.red = true;
+    }
+
+    public int size() {
+        return size;
+    }
+}
---
+++ src/main/java/rbtree/TreeTest.java
@@ -0,0 +1,37 @@
+package rbtree;
+
+/**
+ * @author xiaoyouming
+ * @version 1.0
+ * @since 2020-11-06
+ */
+public class TreeTest {
+    public static void main(String[] args) {
+        RBTree<Integer> tree = new RBTree<>();
+        tree.insert(450);
+        tree.insert(250);
+        tree.insert(150);
+        tree.insert(50);
+        tree.insert(25);
+        tree.insert(100);
+        tree.insert(220);
+        tree.insert(200);
+        tree.insert(230);
+        tree.insert(350);
+        tree.insert(300);
+        tree.insert(400);
+        tree.insert(650);
+        tree.insert(550);
+        tree.insert(480);
+        tree.insert(470);
+        tree.insert(460);
+        tree.insert(500);
+        tree.insert(600);
+        tree.insert(750);
+        tree.insert(700);
+        tree.insert(650);
+        tree.insert(800);
+
+        System.out.println("aa");
+    }
+}
